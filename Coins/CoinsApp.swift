b2bb015bcb3b710b4238f4734247ff2cde1f5fbc
Bekbol Bolatov <bekbolbolatov@gmail.com>
2024-01-04 18:24:26 +0600

    Initial Commit

---
+++ Coins/CoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsApp.swift
+//  Coins
+//
+//  Created by Bekbol Bolatov on 04.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
